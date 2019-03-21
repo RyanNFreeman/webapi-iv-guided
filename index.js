c64e75b080f53babfeef3dd4830931d914d91668
RyanNFreeman <ryan.norman.freeman@gmail.com>
2019-03-21 11:54:06 -0600

    really

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
+// require('dotenv').config()
 const server = require('./api/server.js');
 
 //make the port dynamic
 const port = process.env.PORT || 4000;
-const greeting = process.env.GREETING;
+const greeting = process.env.GREETING || 'Hello world';
 server.listen(port, () => {
   console.log(`\n*** ${greeting} http://localhost:${port} ***\n`);
 });
